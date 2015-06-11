b760deb4536e8cdfe091dec7f7872802e8bedaab
stevegocoding <stevegocoding@gmail.com>
2015-06-11 12:30:30 -0400

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to my-4clojure-solutions
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/my_4clojure_solutions/core.clj
@@ -0,0 +1,7 @@
+(ns my-4clojure-solutions.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
